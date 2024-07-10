30af14df2ea926d0e7e04c8d2f5d717ce79571c0
Ken-K12 <k12major28k@gmail.com>
2024-07-10 22:51:28 +0900

    Cfn Pythonスクリプトの修正

--- IaC/run-task.sh
+++ IaC/run-task.sh
@@ -1,13 +1,17 @@
-clustername=$1
-taskdefinitionname=$2
+CLUSTER_NAME=$1
+TASK_DEFINITION=$2
+SUBNET_ID=$3
+SECURITY_GROUP_ID=$4
 
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
     echo "引数を指定してください"
     exit 1
 fi
 
 aws ecs run-task \
-    --cluster  $1\
-    --task-definition $2 \
+    --cluster $CLUSTER_NAME \
+    --task-definition $TASK_DEFINITION \
     --count 1 \
-    --launch-type FARGATE
+    --launch-type FARGATE \
+    --network-configuration \
+    "awsvpcConfiguration={subnets=[$SUBNET_ID],securityGroups=[$SECURITY_GROUP_ID],assignPublicIp=ENABLED}"
--- app/get_kobe_info.py
+++ app/get_kobe_info.py
@@ -1,30 +1,77 @@
 import os
 import re
 import csv
-
+import logging
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from functools import wraps
+
+# Load environment variables
+load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 url_list = ['https://www.feel-kobe.jp/event/']
+access_token = os.getenv('LINE_NOTIFY_TOKEN')
 
-def send_line(message, token):
-    url = 'https://notify-api.line.me/api/notify'
-    headers = {
-        'Authorization': f'Bearer {token}'
-    }
-    data = {
-        'message': message
-    }
-    response = requests.post(url, headers=headers, data=data)
-    return response.status_code
+# Decorator for logging function entry and exit
+def log_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kargs):
+        logger.info(f'Start {func.__name__}')
+        result = func(*args, **kargs)
+        logger.info(f'End {func.__name__}')
+        return result
+    return wrapper
 
-def write_csv(href):
-    with open('event_url.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([])
 
+class LineNotify:
+    def __init__(self, token):
+        self.token = token
+
+    @log_decorator
+    def send_line(self, message):
+        url = 'https://notify-api.line.me/api/notify'
+        headers = {
+            'Authorization': f'Bearer {self.token}'
+        }
+        data = {
+            'message': message
+        }
+        response = requests.post(url, headers=headers, data=data)
+        return response.status_code
+
+
+class CSVOperation:
+    def __init__(self, filename):
+        self.filename = filename
+
+    @log_decorator
+    def write_csv(self, href):
+        with open(self.filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([href])
+    
+    @log_decorator
+    def check_url_in_csv(self, url):
+        
+        with open(self.filename, 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                logger.info(f'row: {row}, url: {url}')
+                if url in row:
+                    return False
+        return True
+
+@log_decorator
 def main():
+    message = ''
+    lineNotify = LineNotify(access_token)
+    csv_operation = CSVOperation('event.csv')
+
     for i in range(len(url_list)):
 
         response = requests.get(url_list[i])
@@ -38,22 +85,20 @@ def main():
             
             for a_tag in a_tags:
                 href = a_tag.get('href')
+
                 if href and url_pattern.search(href):
-                    print(href)
+                    response = requests.get(href)
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    title = soup.find('title').text
+                    message = f'Title: {title}\n'
+                    message += f'URL : {href}\n'
+                    
+                    if csv_operation.check_url_in_csv(href):
+                        csv_operation.write_csv(href)
+                        lineNotify.send_line(message)
 
         else:
             print(f"Failed to retrieve content. Status code: {response.status_code}")
 
 if __name__ == "__main__":
-
     main()
-    # load_dotenv()
-    # access_token = os.getenv('LINE_NOTIFY_TOKEN')
-    # message = "Hello from Python! This is a test message."
-
-    # status_code = send_line(message, access_token)
-
-    # if status_code == 200:
-    #     print('message sent successfuly.')
-    # else:
-    #     print('failed to send message. Status code: {status_code}')
